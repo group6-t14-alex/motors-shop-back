75f859fb1cac083caf1bca04461bca07b10049c5
eskokado <eskokado@gmail.com>
2023-06-06 14:10:43 -0300

    refactor: add annotation @ApiProperty into Car entity

--- src/modules/cars/entities/car.entity.ts
+++ src/modules/cars/entities/car.entity.ts
@@ -1,15 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Car {
+  @ApiProperty()
   readonly id: number;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   description: string;
+  @ApiProperty()
   km: number;
+  @ApiProperty()
   year: number;
+  @ApiProperty()
   price: number;
+  @ApiProperty()
   priceFipe: number;
+  @ApiProperty()
   imageUrl: string;
+  @ApiProperty()
   model: string;
+  @ApiProperty()
   color: string;
+  @ApiProperty()
   brand: string;
+  @ApiProperty()
   fuel: string;
+  @ApiProperty()
   isActive: boolean;
 }
